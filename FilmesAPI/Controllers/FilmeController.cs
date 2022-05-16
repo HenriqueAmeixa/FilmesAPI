3a49f692fd38690173df026dfe1e046c46afd360
henrique ameixa <henrique-ameixa@hotmail.com>
2022-05-16 16:40:11 -0300

    Ultima att

--- FilmesAPI/Controllers/FilmeController.cs
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -31,10 +31,26 @@ namespace FilmesAPI.Controllers
             return CreatedAtAction(nameof(RecuperaFilmePorId), new {Id = filme.Id},filme);
         }
         [HttpGet]
-        public IActionResult RecuperarFilmes()
+        public IActionResult RecuperarFilmes([FromQuery] int? classificacaoEtaria)
         {
-                return  Ok(_context.Filmes);       
-
+            List<Filme> filmes;
+            if (classificacaoEtaria == null)
+            {
+                filmes = _context.Filmes.ToList();
+                List<ReadFilmeDto> readDto = _mapper.Map<List<ReadFilmeDto>>(filmes);
+                return Ok(readDto);
+            }
+            else
+            {
+                filmes = _context.Filmes.Where(filme => filme.ClassificacaoEtaria <= classificacaoEtaria).ToList();
+                if (filmes != null)
+                {
+                    List<ReadFilmeDto> readDto = _mapper.Map<List<ReadFilmeDto>>(filmes);
+                    return Ok(readDto);
+                }
+            }
+            return NotFound();
+              
         }
         [HttpGet("{id}")]
         [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Filme))]
--- FilmesAPI/Migrations/20220513142143_Classificacao.Designer.cs
+++ FilmesAPI/Migrations/20220513142143_Classificacao.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using FilmesAPI.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace FilmesAPI.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220513142143_Classificacao")]
+    partial class Classificacao
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
